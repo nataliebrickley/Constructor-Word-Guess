85a5970dbe61137592b9c149ebfa6f7e3faf7f56
Natalie Brickley <natt_rose@yahoo.com>
2019-03-21 17:26:52 -0700

    added an array of words, and a game restart function

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Word = require("./word");
 const inquirer = require("inquirer")
-
-let word = new Word("testing");
+let wordPool = ["function", "variable", "language", "array", "object", "constant", "parameter", "hashtag", "number", "string", "boolean"]
+let random = Math.floor(Math.random()*11)
+let word = new Word(wordPool[random]);
+let guessesMade = [];
 word.displayWord()
 let count = 10;
 const play = function () {
@@ -11,21 +13,49 @@ const play = function () {
             message: "Guess a letter!"
         }
     ]).then(function (response) {
-        word.guess(response.guess);
-        //console.log(response)
-        word.displayWord()
-        count --
-        console.log(count + " guesses remaining")
-        if (count > 0 && !word.solved) {
-            play()
+        if (guessesMade.includes(response.guess)) {
+            console.log("You already guessed that letter! Try Again.")
+            play();
         }
-        else if (word.solved){
-            console.log("CONGRATULATIONS! You Win!!!")
+        else {
+            guessesMade.push(response.guess)
+            word.guess(response.guess);
+            //console.log(response)
+            word.displayWord()
+            count--
+            console.log(count + " guesses remaining")
+            if (count > 0 && !word.solved) {
+                play()
+            }
+            else if (word.solved) {
+                console.log("CONGRATULATIONS! You Win!!!")
+                newGame();
+            }
+            else {
+                console.log("Sorry you ran out of guesses!")
+                newGame();
+            }
         }
-        else{
-            console.log("Sorry you ran out of guesses!")
-        }
-        
+
     })
 }
 play();
+
+const newGame = function() {
+    inquirer.prompt([
+        {
+            type: "confirm",
+            name: "restart",
+            message: "Play Again?"
+        }
+    ]).then(function(response) {
+        if(response.restart) {
+            random = Math.floor(Math.random()*11);
+            word = new Word(wordPool[random]);
+            guessesMade = [];
+            word.displayWord();
+            count = 10;
+            play();
+        }
+    })
+}
